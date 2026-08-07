d0649bb533451a199211083386e7d64503350e79
zhuchara-oleg <zhucharalytiy@gmail.com>
2026-08-07 15:29:04 +0300

    Add files via upload

---
+++ nike header/js/index.js
@@ -0,0 +1,29 @@
+import HeaderFixed from "./header.js";
+import BurgerMenu from "./burger.js";
+
+try {
+  const headerFixed = new HeaderFixed({
+    HEADER: "header",
+    HEADER_FIXED: "header--fixed",
+  });
+
+  new BurgerMenu(
+    {
+      BURGER: "burger",
+      BURGER_OPEN: "burger--open",
+      HEADER_MENU: "header__menu",
+      HEADER_MENU_OPEN: "header__menu--open",
+      lABEL: {
+        OPEN: "Открыть меню",
+        CLOSE: "Закрыть меню",
+      },
+      PAGE_BODY: "page__body",
+      PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+      MENU_LINK: "menu__link",
+      BREAKPOINT: 768,
+    },
+    headerFixed
+  );
+} catch (error) {
+  console.error(error);
+}
